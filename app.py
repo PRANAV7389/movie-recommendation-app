1e6ccdf5c3f32fd8e23807394bcafaaf88868aff
Pranav Sharma <110465335+PRANAV7389@users.noreply.github.com>
2024-12-31 01:59:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,7 @@ Welcome to the Movie Recommendation System! Enter your favorite movie, and we'll
 # Preloaded dataset
 movies_data = pd.read_csv("movies.csv")
 
-# Show the first few rows of the dataset
-st.subheader("Dataset Preview")
-st.dataframe(movies_data.head())
-
 # Data Preprocessing
-st.subheader("Data Preprocessing")
 selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
 for feature in selected_features:
     movies_data[feature] = movies_data[feature].fillna('')
@@ -34,31 +29,31 @@ feature_vectors = vectorizer.fit_transform(combined_features)
 # Cosine Similarity
 similarity = cosine_similarity(feature_vectors)
 
-# User Input
-movie_name = st.text_input("Enter your favorite movie name:")
-if movie_name:
-    list_of_all_titles = movies_data['title'].tolist()
-    find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
-    if find_close_match:
-        close_match = find_close_match[0]
+# User Input with Dropdown
+list_of_all_titles = movies_data['title'].tolist()
+movie_name = st.selectbox("Enter your favorite movie name:", options=["Select a movie"] + list_of_all_titles)
+if movie_name and movie_name != "Select a movie":
+    # Get the index of the movie
+    index_of_the_movie = movies_data[movies_data.title == movie_name]['index'].values[0]
 
-        st.write(f"Close match found: **{close_match}**")
+    # Get similar movies
+    similarity_score = list(enumerate(similarity[index_of_the_movie]))
+    sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
 
-        # Get the index of the movie
-        index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
-
-        # Get similar movies
-        similarity_score = list(enumerate(similarity[index_of_the_movie]))
-        sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-
-        # Display recommended movies
-        st.subheader("Movies recommended for you:")
-        recommended_movies = []
-        for i, movie in enumerate(sorted_similar_movies):
-            index = movie[0]
-            title_from_index = movies_data[movies_data.index == index]['title'].values[0]
-            if i < 30:
-                recommended_movies.append(title_from_index)
-        st.write(recommended_movies)
-    else:
-        st.error("No close match found. Please try a different movie name.")
+    # Display recommended movies in a table
+    st.subheader("Movies recommended for you:")
+    recommended_movies = []
+    for i, movie in enumerate(sorted_similar_movies):
+        index = movie[0]
+        if i < 30:
+            recommended_movies.append({
+                "Title": movies_data[movies_data.index == index]['title'].values[0],
+                "Genre": movies_data[movies_data.index == index]['genres'].values[0],
+                "Cast": movies_data[movies_data.index == index]['cast'].values[0],
+                "Director": movies_data[movies_data.index == index]['director'].values[0]
+            })
+    
+    recommended_df = pd.DataFrame(recommended_movies)
+    st.table(recommended_df)
+else:
+    st.info("Please select a movie to get recommendations.")
